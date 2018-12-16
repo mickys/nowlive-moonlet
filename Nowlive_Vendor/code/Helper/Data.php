3f0e6838f008d801daee1a50bd6acf696b60da87
Micky Socaci <micky@nowlive.ro>
2018-12-16 12:59:25 +0200

    Nowlive Vendor module, that bundles web3.php

---
+++ Nowlive_Vendor/code/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class Nowlive_Vendor_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 
---
+++ Nowlive_Vendor/code/Model/Observer.php
@@ -0,0 +1,22 @@
+<?php
+ 
+class Nowlive_Vendor_Model_Observer
+{
+    public function controllerFrontInitBefore(Varien_Event_Observer $event)
+    {
+        self::init();
+    }
+ 
+    /**
+     * Add in auto loader for Elasticsearch components
+     */
+    static function init()
+    {
+        // Add our vendor folder to our include path
+        set_include_path(get_include_path() . PATH_SEPARATOR . Mage::getBaseDir('lib') . DS . 'Nowlive' . DS . 'web3.php' . DS . 'vendor');
+ 
+        // Include the autoloader for composer
+        require_once(Mage::getBaseDir('lib') . DS . 'Nowlive' . DS . 'web3.php' . DS . 'vendor' . DS . 'autoload.php');
+    }
+ 
+}
